60df228f16daa8aaa935b70c2def3477d1da877d
Ole Andre Birkedal <birkedal85@gmail.com>
2016-08-16 14:42:25 +0200

    No more mnesia

--- lib/database.ex
+++
@@ -1,7 +0,0 @@
-use Amnesia
-
-defdatabase Jodelx.Database do
-  deftable Post, [:created_at, :vote_count, :location_name,
-                  :child_count, :color, :user_handle, :image_url, :message] do
-  end
-end
