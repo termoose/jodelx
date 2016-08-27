e16916a9babd39b4d37a52b41aa6d094319d0399
Ole Andre Birkedal <birkedal85@gmail.com>
2016-08-27 22:18:12 +0200

    Added additional fields to reply table

--- lib/jodelx/reply.ex
+++ lib/jodelx/reply.ex
@@ -11,13 +11,15 @@ defmodule Jodelx.Reply do
     field :user_handle, :string
     field :message, :string
     field :img_url, :string
+    field :parent_creator, :boolean
+    field :parent_post_id, :string
   end
 
   def changeset(reply, params \\ %{}) do
     reply
     |> cast(params,
             [:created_at, :color, :location, :post_id, :vote_count,
-             :user_handle, :message, :img_url])
+             :user_handle, :message, :img_url, :parent_creator, :parent_post_id])
     |> unique_constraint(:post_id)
   end
 end
--- priv/repo/migrations/20160827195519_create_replies.exs
+++ priv/repo/migrations/20160827195519_create_replies.exs
@@ -11,6 +11,8 @@ defmodule Jodelx.Repo.Migrations.CreateReplies do
       add :user_handle, :string
       add :message, :string
       add :img_url, :string
+      add :parent_creator, :boolean
+      add :parent_post_id, :string
     end
 
     create unique_index(:reply, [:post_id])
