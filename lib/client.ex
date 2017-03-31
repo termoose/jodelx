99c0d336e27574638160e7ecc6639fc586990b9b
Ole Andre Birkedal <birkedal85@gmail.com>
2017-03-31 12:27:46 +0200

    New decompiled jodel api key

--- lib/client.ex
+++ lib/client.ex
@@ -4,12 +4,12 @@ defmodule Jodelx.Client do
   @base_api "api.go-tellm.com"
   @api_path "/api/v2/"
   @endpoint "https://"<> @base_api <> @api_path
-  @user_agent "Jodel/4.38.3 Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus 5 Build/MMB29V)"
+  @user_agent "Jodel/4.39.1 Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus 5 Build/MMB29V)"
   #@secret "pNsUaphGEfqqZJJIKHjfxAReDqdCTIIuaIVGaowG"
   #@secret "iyWpGGuOOCdKIMRsfxoJMIPsmCFdrscSxGyCfmBb"
-  @secret "KZmLMUggDeMzQfqMNYFLWNyttEmQgClvlPyACVlH"
+  @secret "GNFlFdFQIJPLtkawtqyPdCeovqdAVokZoNGFgCfv"
   @client_id "81e8a76e-1e02-4d17-9ba0-8a7020261b26"
-  @client_type "android_4.38.3"
+  @client_type "android_4.39.1"
   @api_version "0.2"
   @alphabet "abcdefghijklmnopqrstuvwxyz123456789"
 
